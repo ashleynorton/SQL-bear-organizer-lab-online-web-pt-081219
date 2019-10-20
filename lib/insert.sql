48f6ccb1758d66ec1c7af89f892523f89d6dc9cf
Ashley Norton <nortonashleym@gmail.com>
2019-10-20 21:52:20 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 15, "M", "brown", "grouchy", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Rowdy", 15, "M", "brown", "grouchy", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Tabitha", 15, "F", "brown", "grouchy", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Sergeant Brown", 15, "F", "brown", "grouchy", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Melissa", 15, "F", "brown", "grouchy", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 15, "F", "brown", "grouchy", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 15, "F", "brown", "grouchy", 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 15, "F", "brown", "grouchy", 0);
